15f152219e77e21fd60d32f168d08a0ca8538ba5
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 09:59:28 -0300

    Melhorando o programa utilizando loço while infinito.O programa será interrompido quando o número solicitado for negativo.

--- Tabuada.py
+++ Tabuada.py
@@ -1,7 +1,14 @@
-'''Refazendo o programa utilizando loço for.'''
+'''Melhorando o programa utilizando loço while infinito.O programa será interrompido quando o número solicitado for negativo.'''
 
-n = int(input('Digite um número inteiro: '))
-print('='*11)
-for c in range(1, 11):
-    print('{} x {:2} = {}'.format(n, c, n * c))
-print('='*11)
+print('-=' * 20)
+print('{:^40}'.format('TABUADA 3.0'))
+print('-=' * 20)
+while True:
+    n = int(input('Quer ver a tabuada de qual número? '))
+    if n<=0:
+        break
+    print('='*40)
+    for c in range(1, 11):
+        print(f'{n} x {c:2} = {n * c}')
+    print('='*40)
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
