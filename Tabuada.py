ce6bb1a6ae25ffe7ce9483e6b5413c1a7a2d0e85
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 09:56:12 -0300

    Refazendo o programa utilizando loço for.

--- Tabuada.py
+++ Tabuada.py
@@ -1,14 +1,7 @@
-'''Programa que ler um número inteiro qualquer e mostre na tela a sua tabuada.'''
+'''Refazendo o programa utilizando loço for.'''
+
 n = int(input('Digite um número inteiro: '))
 print('='*11)
-print('{} x  1 = {}'.format(n, n * 1))
-print('{} x  2 = {}'.format(n, n * 2))
-print('{} x  3 = {}'.format(n, n * 3))
-print('{} x  4 = {}'.format(n, n * 4))
-print('{} x  5 = {}'.format(n, n * 5))
-print('{} x  6 = {}'.format(n, n * 6))
-print('{} x  7 = {}'.format(n, n * 7))
-print('{} x  8 = {}'.format(n, n * 8))
-print('{} x  9 = {}'.format(n, n * 9))
-print('{} x 10 = {}'.format(n, n * 10))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(n, c, n * c))
 print('='*11)
